36b4a8261aa6afc9417f0c6b4fe59dab6cc5c465
AnjneyMishraTesting <anjneymishra21@gmail.com>
2024-09-23 11:34:19 +0530

    NextUI Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import {Button, ButtonGroup} from "@nextui-org/button";
 function App() {
 
   return (
     <>
     <h1 className="font-bold text-4xl">Hello World from my project</h1>
+    <Button color="danger">
+      Button
+    </Button>
     </>
   )
 }
